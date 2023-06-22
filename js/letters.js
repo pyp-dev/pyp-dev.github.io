bde67c773387410a88e91a6b3fc68be70ae07d33
parrotypoisson <71202170+parrotypoisson@users.noreply.github.com>
2023-06-22 14:01:48 -0500

    Updated lttrly

--- js/letters.js
+++ js/letters.js
@@ -1,47 +1,61 @@
-export { getRandomCategories, getRandomCategory, getRandomLetter };
+export { getRandomCategories, getRandomCategory, getRandomValue };
 
 const categories = [
-    ['vowels', ['a','e','i','o','u']],
-    ['vowels with y', ['a','e','i','o','u','y']],
-    ['wheel of fortune given', ['r','s','t','l','n','e']],
-    ['rainbow', ['r','o','y','g','b','i','v']],
-    ['compass', ['n','s','e','w']],
-    ['homerow', ['a','s','d','f','j','k','l']],
-    ['common keyboard', ['q','w','e','r','t','y']],
-    ['alt keyboard', ['d','v','o','r','a','k']],
-    ['clothes sizes', ['x','s','m','l']],
-    ['grades', ['a','b','c','d','f']],
-    ['scrabble uncommon', ['k','j','x','q','z']],
-    ['temperature scales', ['c','f','k']],
-    ['priorities', ['h','m','l']],
-    ['game controller', ['a','b','x','y']],
-    ['pc gaming', ['w','a','s','d']],
-    ['karate belts', ['w','y','o','g','p','b']],
-    ['college degrees', ['a','b','m','d']],
-    ['dna bases', ['a','c','g','t']],
-    ['rna bases', ['a','c','g','u']],
-    ['hogwarts houses', ['g','h','r','s']],
-    ['zodiac signs', ['a','t','g','c','l','v','s','p']],
-    ['usa time zones', ['e','c','m','p','a','h']],
-    ['movie ratings', ['g','p','r','n']],
-    ['game ratings', ['e','t','m','a']],
-    ['dallas sports teams', ['c','m','s','r']],
-    ['new orleans sports teams', ['s','p']],
-    ['houston sports teams', ['t','r','a']],
-    ['phoenix sports teams', ['c','s','d']],
-    ['denver sports teams', ['b','n','a','r']],
-    ['kansas city sports teams', ['c','r']],
-    ['indianapolis sports teams', ['c','p']],
-    ['cincinnati sports teams', ['b','r']],
-    ['minneapolis sports teams', ['v','t','w']],
-    ['detroit sports teams', ['l','p','r','t']],
-    ['cleveland sports teams', ['b','c','g']],
-    ['atlanta sports teams', ['f','h','b']],
-    ['olympic rings', ['b','y','g','r']],
-    ['m&m colors', ['r','o','g','b','y']],
-    ['skittles colors', ['p','y','g','o','r']],
-    ['froot loops colors', ['r','o','y','g','b','p']],
-    
+    ['Vowels', ['A','E','I','O','U']],
+    ['Vowels Incl. Y', ['A','E','I','O','U','Y']],
+    ['Wheel of Fortune Given', ['R','S','T','L','N','E']],
+    ['Rainbow', ['Red','Orange','Yellow','Green','Blue','Indigo','Violet']],
+    ['Compass', ['North','South','East','West']],
+    ['Homerow Keyboard', ['A','S','D','F','J','K','L']],
+    ['Common Keyboard Layout', ['Q','W','E','R','T','Y']],
+    ['Alt Keyboard Layout', ['D','V','O','R','A','K']],
+    ['Clothes Sizes', ['Small','Medium','Large','X-Large']],
+    ['Grades', ['A','B','C','D','F']],
+    ['Scrabble Uncommon', ['K','J','X','Q','Z']],
+    ['Temperature Scales', ['Celsius','Fahrenheit','Kelvin']],
+    ['Xbox Controller Buttons', ['A','B','X','Y']],
+    ['PC Movement', ['W','A','S','D']],
+    ['College Degree Levels', ['Associate','Bachelor','Master','Doctoral']],
+    ['DNA Bases', ['Adenine','Cytosine','Guanine','Thymine']],
+    ['RNA Bases', ['Adenine','Cytosine','Guanine','Uracil']],
+    ['Hogwarts Houses', ['Gryffindor','Hufflepuff','Ravenclaw','Slytherin']],
+    ['Zodiac Signs', ['Aries','Taurus','Gemini','Cancer','Leo','Virgo','Libra','Scorpio','Sagittarius','Capricorn','Aquarius','Pisces']],
+    ['U.S. Time Zones', ['Eastern','Central','Mountain','Pacific','Alaskan','Hawaiian']],
+    ['Movie Ratings', ['G','PG','PG-13','R','NC-17']],
+    ['Game Ratings', ['Everyone','Teen','Mature','Adults Only']],
+    ['Dallas Sports Teams', ['Cowboys','Mavericks','Stars','Rangers']],
+    ['New Orleans Sports Teams', ['Saints','Pelicans']],
+    ['Houston Sports Teams', ['Texans','Rockets','Astros']],
+    ['Phoenix Sports Teams', ['Cardinals','Suns','Coyotes','Diamondbacks']],
+    ['Denver Sports Teams', ['Broncos','Nuggets','Avalanche','Rockies']],
+    ['Kansas City Sports Teams', ['Chiefs','Royals']],
+    ['Indianapolis Sports Teams', ['Colts','Pacers']],
+    ['Cincinnati Sports Teams', ['Bengals','Reds']],
+    ['Minneapolis Sports Teams', ['Vikings','Timberwolves','Wild','Twins']],
+    ['Detroit Sports Teams', ['Lions','Pistons','Red Wings','Tigers']],
+    ['Cleveland Sports Teams', ['Browns','Cavaliers','Guardians']],
+    ['Atlanta Sports Teams', ['Falcons','Hawks','Braves']],
+    ['Olympic Rings', ['Blue','Yellow','Black','Green','Red']],
+    ['M&M\'s Colors', ['Red','Orange','Green','Blue','Brown','Yellow']],
+    ['Skittles Colors', ['Purple','Yellow','Green','Orange','Red']],
+    ['Froot Loops Colors', ['Red','Orange','Yellow','Green','Blue','Purple']],
+    ['Roulette Colors', ['Red','Black','Green']],
+    ['Seasons', ['Spring','Summer','Winter','Fall']],
+    ['Great Lakes', ['Superior','Michigan','Huron','Erie','Ontario']],
+    ['Oceans', ['Pacific','Atlantic','Indian','Southern','Arctic']],
+    ['Continents', ['Asia','Africa','North America','South America','Antarctica','Europe','Austrlia']],
+    ['Card Suits', ['Clubs','Spades','Hearts','Diamonds']],
+    ['U.S. Coins', ['Penny','Nickel','Dime','Quarter','Half Dollar','Dollar']],
+    ['U.S. Paper Money', ['One','Two','Five','Ten','Twenty','Fifty','One Hundred']],
+    ['Four Corner States', ['Utah','Colorado','New Mexico','Arizona']],
+    ['Lucky\'s Charms', ['Heart','Star','Horseshoe','Clover','Blue Moon','Unicorn','Rainbow','Red Balloon']],
+    ['Countries of North America', ['Canada','United States','Mexico']],
+    ['Automatic Car Gears', ['Park','Reverse','Drive','Neutral']],
+    ['Chess Pieces', ['King','Queen','Bishop','Knight','Rook','Pawn']],
+    ['U.S. Flag Colors', ['Red','White','Blue']],
+    ['Tic-Tac-Toe Symbols', ['X','O']],
+    ['Seattle Sports Teams', ['Seahawks','Kraken','Mariners']],
+    ['Original Monopoly Pieces', ['Battleship','Boot','Cannon','Thimble','Top Hat','Iron']]
 ];
 
 function getCategoryNames(){
@@ -50,7 +64,7 @@ function getCategoryNames(){
 
 function getRandomCategories(){
     const chosen = new Set()
-    for(let i = 0; i<10; i++){
+    for(let i = 0; i<9; i++){
         chosen.add(categories[Math.floor(Math.random()*categories.length)]);
     }
     
@@ -61,6 +75,6 @@ function getRandomCategory(categories){
     return categories[Math.floor(Math.random()*categories.length)];
 }
 
-function getRandomLetter(values){
+function getRandomValue(values){
     return values[Math.floor(Math.random()*values.length)];
 }
--- js/lttrly.js
+++ js/lttrly.js
@@ -1,12 +1,23 @@
-import {getRandomCategories, getRandomCategory, getRandomLetter} from "./letters.js";
+import { getRandomCategories, getRandomCategory, getRandomValue } from "./letters.js";
 
 let categories = null;
 let categoryName = null;
 let categoryValues = null;
+let categoryLetters = null;
+let value = null
 let letter = null;
 let remaining = 5;
 let wins = 0
 let games = 0
+
+//
+// Creation
+//
+function createDisplay(){
+    createGuesses();
+    createKeyboard();
+}
+
 
 function createGuesses(){
     const guesses = document.getElementById('guesses');
@@ -18,69 +29,29 @@ function createGuesses(){
     }
 }
 
+
 function createKeyboard(){
     const keyboard = document.getElementById('keyboard');
 }
 
-function checkGuess(){
-    let guess = document.getElementsByClassName('guess')[5 - remaining];
-    let key = findKey(guess.innerHTML);
-    
-    if(guess.innerHTML == letter){
-        guess.classList.add('correct');
-        key.classList.add('correct');
-        displayResult(true);
-        remaining = 0;
-        return;
-    }
-    else if(categoryValues.includes(guess.innerHTML)){
-        guess.classList.add('close');
-        key.classList.add('close');
-    }
-    else{
-        key.classList.add('wrong');
-    }
-    
-    remaining -= 1;
-    if(remaining <= 0){
-        displayResult(false);
-    }
+
+
+//
+// Reset
+//
+function resetDisplay(){
+    resetCategories();
+    resetGuesses();
+    resetResult();
+    resetKeyboard();
 }
 
-function findKey(key){
-    const keys = document.getElementsByClassName('key');
-    
-    for(let i = 0; i<keys.length; i++){
-        if(keys[i].innerHTML == key){
-            return keys[i];
-        }
-    }
-}
-
-function guessedAlready(letter){
-    const guesses = document.getElementsByClassName('guess');
-    
-    for(let i = 0; i<guesses.length; i++){
-        if(guesses[i].innerHTML === letter){
-            return true;
-        }
-    }
-    
-    return false;
-}
-
-function displayResult(won){
-    const result = document.getElementById('result');
-    result.innerText = `You ${won ? 'Won!' : 'Lost.'} The category was ${categoryName}, and the letter was ${letter.toUpperCase()}.`;
-    result.style.color = won ? 'green' : 'red';
-    result.style.visibility = 'visible';
-    updateRecord(won);
-}
 
 function resetCategories(){
     const cats = document.getElementById('categories');
     cats.innerText = '';
 }
+
 
 function resetGuesses(){
     const guess_list = document.getElementsByClassName('guess');
@@ -90,23 +61,39 @@ function resetGuesses(){
         guess.innerText = '';
         guess.classList.remove('correct','close');
     }
-    
+
     remaining = 5;
 }
+
 
 function resetResult(){
     const result = document.getElementById('result');
     result.style.visibility = 'hidden';
 }
 
+
 function resetKeyboard(){
     const key_list = document.getElementsByClassName('key');
-    
+
     for(let i = 0; i<key_list.length; i++){
         let key = key_list[i];
         key.classList.remove('close','correct','wrong');
     }
 }
+
+
+
+//
+// Update Display
+//
+function displayResult(won){
+    const result = document.getElementById('result');
+    result.innerText = `You ${won ? 'Won!' : 'Lost.'} The category was ${categoryName}, and the letter was ${letter + (value!==letter ? '['+value.slice(1)+']' : '')}.`;
+    result.style.color = won ? 'green' : 'red';
+    result.style.visibility = 'visible';
+    updateRecord(won);
+}
+
 
 function updateRecord(won){
     games++;
@@ -114,11 +101,18 @@ function updateRecord(won){
     document.getElementById('record').innerText = `${wins}/${games}`;
 }
 
+
+
+//
+// Pick Letter
+//
 function chooseLetter(){
     categories = getRandomCategories();
     [categoryName, categoryValues] = getRandomCategory(categories);
-    letter = getRandomLetter(categoryValues);
-    
+    categoryLetters = categoryValues.map(x=>x[0]);
+    value = getRandomValue(categoryValues);
+    letter = value[0];
+
     const cats = document.getElementById('categories');
     const list = document.createElement('ul');
     for(let x of categories){
@@ -132,26 +126,87 @@ function chooseLetter(){
         });
         list.appendChild(element);
     }
-    
+
     cats.appendChild(list);
 }
 
-function setupGame(){
-    chooseLetter();
-    createGuesses();
-    createKeyboard();
+
+
+//
+// Logic Helpers
+//
+function checkGuess(){
+    let guess = document.getElementsByClassName('guess')[5 - remaining];
+    let key = findKey(guess.innerHTML);
+
+    if(guess.innerHTML === letter){
+        guess.classList.add('correct');
+        key.classList.add('correct');
+        displayResult(true);
+        remaining = 0;
+        return;
+    }
+    else if(categoryLetters.includes(guess.innerHTML)){
+        guess.classList.add('close');
+        key.classList.add('close');
+    }
+    else{
+        key.classList.add('wrong');
+    }
+
+    remaining -= 1;
+    if(remaining <= 0){
+        displayResult(false);
+    }
 }
 
-function newGame(){
-    resetCategories();
-    chooseLetter();
-    resetGuesses();
-    resetResult();
-    resetKeyboard();
+
+function findKey(key){
+    const keys = document.getElementsByClassName('key');
+
+    for(let i = 0; i<keys.length; i++){
+        if(keys[i].innerHTML === key){
+            return keys[i];
+        }
+    }
+}
+
+
+function guessedAlready(letter){
+    const guesses = document.getElementsByClassName('guess');
+
+    for(let i = 0; i<guesses.length; i++){
+        if(guesses[i].innerHTML === letter){
+            return true;
+        }
+    }
+
+    return false;
 }
 
 
 
+//
+// Initialize Game
+//
+function restartGame(){
+    if(remaining>0){
+        updateRecord();
+    }
+    resetDisplay();
+    chooseLetter();
+}
+
+function firstGame(){
+    createDisplay();
+    chooseLetter();
+}
+
+
+
+//
+// Event Listeners
+//
 document.addEventListener('keyup', (e) => {
     if(remaining>0 && !guessedAlready(e.key)){
         document.getElementsByClassName('guess')[5-remaining].textContent = e.key;
@@ -159,14 +214,19 @@ document.addEventListener('keyup', (e) => {
     }
 })
 
+
 document.getElementById('keyboard').addEventListener('click', e => {
     document.dispatchEvent(new KeyboardEvent('keyup', {'key': e.target.textContent}));
 })
 
+
 document.getElementById('newgame').addEventListener('click', e => {
-    newGame();
+    restartGame();
 })
 
 
 
-setupGame();
+//
+// Entry Point
+//
+firstGame();
